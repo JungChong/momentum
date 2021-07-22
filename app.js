430899caeb52bcdd9b5215517da0e06a0c00d580
JungChong <87227506+JungChong@users.noreply.github.com>
2021-07-23 03:27:09 +0900

    #3.7

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-title.innerText = "Hello";
+function handleh1Click() {
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  console.log(newColor);
+  h1.style.color = newColor;
+}
+
+h1.addEventListener("click", handleh1Click);
